0432163833813299c5b37cabf49e31d925e011c2
Nikita Panyuhin <n.panuhin@mail.ru>
2021-11-10 01:28:36 +0300

    Added custom classes: Scanner, IntList

---
+++ MyClasses/IntList.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+
+public class IntList {
+    public int[] numbers = new int[0];
+    public int size = 0;
+
+    public void set(int pos, int value) {
+        while (pos >= this.numbers.length) {
+            this.numbers = Arrays.copyOf(this.numbers, Math.max(this.numbers.length * 2, 1));
+        }
+        this.numbers[pos] = value;
+        this.size = Math.max(this.size, pos + 1);
+    }
+
+    public void add(int value) {
+        set(size, value);
+    }
+
+    public int get(int pos) {
+        return this.numbers[pos];
+    }
+}
